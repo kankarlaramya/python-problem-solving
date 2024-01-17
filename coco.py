10fd19ced4f51a4ef28fd9453b27f200f0302d2e
kankarlaramya <151029775+kankarlaramya@users.noreply.github.com>
2024-01-17 16:44:14 +0530

    Create coco.py
    In summary, the program determines eligibility based on the user's name and age, allowing them to watch the movie "Coco" if their name starts with 'b' or 'B' and they are 10 years or older. Otherwise, it informs them that they are not eligible.

---
+++ coco.py
@@ -0,0 +1,12 @@
+# ask user's name and age
+# if user's name start with('a' 'A')
+# and age is above 10 then
+# print "you can watch coco movie"
+# else print u can't watch
+#solution
+name = input ("enter your name:")
+age = int(input ("enter your age:"))
+if (name [0]=='b'or name[0]== 'B') and age>=10:
+    print ("you can watch coco movies")
+else:
+    print ( " sorry u can't watch coco")    
