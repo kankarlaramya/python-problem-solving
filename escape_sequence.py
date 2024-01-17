0e2383054dc389b22886029d77377e5b5bb1343f
kankarlaramya <151029775+kankarlaramya@users.noreply.github.com>
2024-01-17 16:39:40 +0530

    Create escape_sequence.py
    Certainly! The provided code includes various examples of escape sequences in Python. Escape sequences are special characters preceded by a backslash (\) that are used to represent certain non-printable or special characters. 
    
    python

---
+++ escape_sequence.py
@@ -0,0 +1,13 @@
+# escape sequence 
+
+print ("line:1\n line:2\n line:3")
+
+# we are using \n to print line in next line
+
+
+print (  " line A \n line B \n  line C  ")
+print ( " sweet\thome ")
+print("\"\"")
+print ( "\\\\")
+print( "/\\ /\\ /\\ /\\")
+print ( "\\ \" \\\ t\\\n \\\'")
